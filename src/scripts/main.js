9cdab9eccfa655e182453d6abed16b210a0aea5b
Roger Santos <rogersw06@gmail.com>
2024-07-18 20:13:32 -0300

    Add files via upload

---
+++ src/scripts/main.js
@@ -0,0 +1,33 @@
+AOS.init();
+
+const dataDoEvento = new Date("Jan 27, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    //console.log(dataDoEvento.getFullYear)
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    //document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+    document.getElementById('seconds').innerHTML = `${segundosAteOEvento}`;
+    document.getElementById('minutes').innerHTML = `${minutosAteOEvento}`;
+    document.getElementById('hours').innerHTML = `${horasAteOEvento}`;
+    document.getElementById('days').innerHTML = `${diasAteOEvento}`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('timeToStart').innerHTML = 'Expired Event';
+    }
+
+}, 1000);
